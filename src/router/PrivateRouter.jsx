9a96abc468a16cde97cbb6ec747602048b602bec
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2024-09-08 16:57:54 +0300

    private router replace

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -4,7 +4,7 @@ import { useAuthContext } from "../context/AuthContext";
 const PrivateRouter = () => {
   const { currentUser } = useAuthContext();
 
-  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+  return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRouter;
